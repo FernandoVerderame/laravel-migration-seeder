7a9caaacc3b250fb3c955f89abf1b8a9529b9458
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-12 16:13:05 +0100

    added trains table

---
+++ database/migrations/2024_03_12_150440_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 12);
+            $table->string('departure_station', 20);
+            $table->string('arrival_station', 20);
+            $table->string('departure_time', 8);
+            $table->string('arrival_time', 8);
+            $table->string('train_code', 6)->unique();
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('canceled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
